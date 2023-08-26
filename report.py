a0d486d373cdf67529f0a8a3e510beb591ef0ad9
LoveNui <yujinxian1212@gmail.com>
2023-08-26 23:13:02 +0900

    Updating rain caculation and add temp at 7:00

--- report.py
+++ report.py
@@ -44,7 +44,34 @@ def get_sum_rain(start_date, end_date):
     k_end = re.findall(r'(\d+)', end_date)
     start = str(k_start[2]) + "." + str(k_start[1]) + "." + str(k_start[0])
     end = str(k_end[2]) + "." + str(k_end[1]) + "." + str(k_end[0])
-    query =f'SELECT SUM(r.MEASVALUE) FROM( SELECT * FROM MEASUREMENTHIST WHERE MEASTIMESTAMP BETWEEN \'{start}, 07:00:00.000\' AND \'{end}, 06:59:59.999\' and MEASID = 10 and MEASVALUE BETWEEN -58 and 122 ORDER BY MEASTIMESTAMP ASC) as r'
+    query_first =f'SELECT Max(r.MEASVALUE) FROM( SELECT * FROM MEASUREMENTHIST WHERE MEASTIMESTAMP BETWEEN \'{start}, 07:00:00.000\' AND \'{start}, 23:59:59.999\' and MEASID = 10 ORDER BY MEASTIMESTAMP ASC) as r'
+    query_second = f'SELECT Max(r.MEASVALUE) FROM( SELECT * FROM MEASUREMENTHIST WHERE MEASTIMESTAMP BETWEEN \'{end}, 00:00:00.000\' AND \'{end}, 07:00:00.000\' and MEASID = 10 ORDER BY MEASTIMESTAMP ASC) as r'
+    rain = 0
+    try:
+        result_first = cur.execute(query=query_first).fetchall()
+        result_second = cur.execute(query=query_second).fetchall()
+        if result_first != [] and result_second != []:
+            if result_first[0][0] != None:
+                rain = rain + result_first[0][0]
+                if result_second[0][0] != None:
+                    rain = rain + result_second[0][0]
+                    return rain
+                else:
+                    return rain
+            elif result_second[0][0] != None:
+                rain = rain + result_second[0][0]
+                return rain
+            else:
+                return None
+        else:
+            return None
+    except:
+        return []
+
+def get_temp_dawn(date):
+    k_date = re.findall(r'(\d+)', date)
+    when = str(k_date[2]) + "." + str(k_date[1]) + "." + str(k_date[0])
+    query =f'SELECT SUM(r.TEMPERATURE) From(SELECT MEASID, AVG(MEASVALUE)as TEMPERATURE FROM MEASUREMENTHIST WHERE MEASTIMESTAMP BETWEEN \'{when}, 06:59:00.000\' AND \'{when}, 07:00:59.999\' and MEASVALUE BETWEEN -58 and 122 Group By MEASID)as r WHERE r.MEASID = 1'
     try:
         result = cur.execute(query=query).fetchall()
         if result != []:
@@ -54,59 +81,60 @@ def get_sum_rain(start_date, end_date):
     except:
         return []
 
+
 def collecting_data(year, month):
     date_list = list_date(year=year, month=month)
     data_temp = []
     data_rain = []
-    df_temp = pd.DataFrame()
+    df_data = pd.DataFrame()
     df_rain = pd.DataFrame()
     date = []
     temp_max = []
     temp_min = []
+    temp_dawn = []
     rain_value = []
     for i in range(len(date_list)-1):
         date.append(date_list[i])
         temp = get_maxmin_temp(date_list[i], date_list[i + 1])
+        dawn = get_temp_dawn(date_list[i])
+        rain = get_sum_rain(date_list[i], date_list[i + 1])
         temp_max.append(temp[0])
         temp_min.append(temp[1])
-        rain = get_sum_rain(date_list[i], date_list[i + 1])
-        rain_value.append(rain[0])
-    df_temp['date'] = date
-    df_temp['max'] = temp_max
-    df_temp['min'] = temp_min
-    df_rain['date'] = date
-    df_rain['rain'] = rain_value
+        temp_dawn.append(dawn[0])
+        rain_value.append(rain)
+    df_data['Date'] = date
+    df_data['Max Temp'] = temp_max
+    df_data['Min Temp'] = temp_min
+    df_data['Temp at 7:00'] = temp_dawn
+    df_data['Rain'] = rain_value
 
-    return df_temp, df_rain
+    return df_data
 
 def save_google_sheet(args):
     year = args.year
     month = args.month
     print(year, month)
-    df_temp, df_rain = collecting_data(year=year, month=month)
+    df_data = collecting_data(year=year, month=month)
     gc = pygsheets.authorize(service_file='cred.json')
     sheet_name = f'Weather Report {year}-{month}'
-    sheet_id = ""
     try:
-        res = gc.sheet.create(sheet_name)
-        sheet_id = res['spreadsheetId']
+        a = gc.open(sheet_name)
+        a.delete()
     except:
         pass
+    res = gc.sheet.create(sheet_name)
+    sheet_id = res['spreadsheetId']
     sh = gc.open_by_key(sheet_id)
     print(f"Created spreadsheet with id:{sh.id} and url:{sh.url}")
     # Share with self to allow to write to it                                                                                                                                                                                                                                                                                         
     sh.share('yujinxian1212@gmail.com', role='writer', type='user')
-    sh = gc.open(sheet_name)
     try:
-        sh.sheet1.title = "Temperature"
-        sh.add_worksheet("Rain")
+        sh.sheet1.title = "Wheather Data"
     except:
         pass
     try:
         temp = sh[0] 
-        temp.set_dataframe(df_temp,(1,1))
-        rain = sh[1]
-        rain.set_dataframe(df_rain,(1,1))
+        temp.set_dataframe(df_data,(1,1))
         return print("Successfuly write on google sheet")
     except:
         return print("Failed in writing on google sheet")
