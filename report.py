ef77111913f8fc6de4c258d970ffa9ba94ca0dbf
LoveNui <yujinxian1212@gmail.com>
2023-08-27 01:03:48 +0900

    Updating rain caculation and add new cloumn for period

--- report.py
+++ report.py
@@ -1,5 +1,6 @@
 import firebirdsql
 from datetime import datetime
+import calendar
 from argparse import ArgumentParser
 import pygsheets
 import pandas as pd
@@ -8,18 +9,25 @@ import csv
 
 conn = firebirdsql.connect(
     host='localhost',
-    database='D:\WeatherMaster-MonthlyReporter\WeatherData.FDB',
+    database='E:\WeatherMaster-MonthlyReporter\WeatherData.FDB',
     user='sysdba',
     password='masterkey'
 )
 cur = conn.cursor()
 
 def list_date(year, month):
-    start_date = datetime.strptime(f'{year}-{month}-01', "%Y-%m-%d")
-    if month == 12:
-        end_date = datetime.strptime(f'{year+1}-01-01', "%Y-%m-%d")
+
+    end_res = calendar.monthrange(year, month)
+    end_day = end_res[1]
+    end_date = datetime.strptime(f'{year}-{month}-{end_day}', "%Y-%m-%d")
+    if month == 1:
+        start_res = calendar.monthrange(year-1, 12)
+        start_day = start_res[1]
+        start_date = datetime.strptime(f'{year-1}-12-{start_day}', "%Y-%m-%d")
     else:
-        end_date = datetime.strptime(f'{year}-{month + 1}-01', "%Y-%m-%d")
+        start_res = calendar.monthrange(year, month -1)
+        start_day = start_res[1]
+        start_date = datetime.strptime(f'{year}-{month-1}-{start_day}', "%Y-%m-%d")
 
     date_list = pd.date_range(start_date, end_date, freq='D').strftime("%Y-%m-%d")
     return date_list.to_list()
@@ -44,28 +52,33 @@ def get_sum_rain(start_date, end_date):
     k_end = re.findall(r'(\d+)', end_date)
     start = str(k_start[2]) + "." + str(k_start[1]) + "." + str(k_start[0])
     end = str(k_end[2]) + "." + str(k_end[1]) + "." + str(k_end[0])
+    query_previous = f'SELECT Max(r.MEASVALUE) FROM( SELECT * FROM MEASUREMENTHIST WHERE MEASTIMESTAMP BETWEEN \'{start}, 00:00:00.000\' AND \'{start}, 06:59:59.999\' and MEASID = 10 ORDER BY MEASTIMESTAMP ASC) as r'
     query_first =f'SELECT Max(r.MEASVALUE) FROM( SELECT * FROM MEASUREMENTHIST WHERE MEASTIMESTAMP BETWEEN \'{start}, 07:00:00.000\' AND \'{start}, 23:59:59.999\' and MEASID = 10 ORDER BY MEASTIMESTAMP ASC) as r'
-    query_second = f'SELECT Max(r.MEASVALUE) FROM( SELECT * FROM MEASUREMENTHIST WHERE MEASTIMESTAMP BETWEEN \'{end}, 00:00:00.000\' AND \'{end}, 07:00:00.000\' and MEASID = 10 ORDER BY MEASTIMESTAMP ASC) as r'
+    query_second = f'SELECT Max(r.MEASVALUE) FROM( SELECT * FROM MEASUREMENTHIST WHERE MEASTIMESTAMP BETWEEN \'{end}, 00:00:00.000\' AND \'{end}, 06:59:59.999\' and MEASID = 10 ORDER BY MEASTIMESTAMP ASC) as r'
     rain = 0
+    flag = 0
     try:
+        result_previous = cur.execute(query=query_previous).fetchall()
         result_first = cur.execute(query=query_first).fetchall()
         result_second = cur.execute(query=query_second).fetchall()
-        if result_first != [] and result_second != []:
+        if result_first != [] and result_second != [] and result_previous != []:
             if result_first[0][0] != None:
                 rain = rain + result_first[0][0]
-                if result_second[0][0] != None:
-                    rain = rain + result_second[0][0]
-                    return rain
-                else:
-                    return rain
-            elif result_second[0][0] != None:
+                flag = 1
+            if result_second[0][0] != None:
                 rain = rain + result_second[0][0]
+                flag = 1
+            if result_previous[0][0] != None:
+                rain = rain - result_previous[0][0]
+                flag = 1
+            if flag == 1:
                 return rain
             else:
                 return None
         else:
             return None
-    except:
+    except Exception as e:
+        print(e)
         return []
 
 def get_temp_dawn(date):
@@ -89,12 +102,14 @@ def collecting_data(year, month):
     df_data = pd.DataFrame()
     df_rain = pd.DataFrame()
     date = []
+    date_period = []
     temp_max = []
     temp_min = []
     temp_dawn = []
     rain_value = []
     for i in range(len(date_list)-1):
-        date.append(date_list[i])
+        date.append(date_list[i+1])
+        date_period.append(f'{date_list[i]}:7:00:00 to {date_list[i + 1]}:6:59:59')
         temp = get_maxmin_temp(date_list[i], date_list[i + 1])
         dawn = get_temp_dawn(date_list[i])
         rain = get_sum_rain(date_list[i], date_list[i + 1])
@@ -107,6 +122,7 @@ def collecting_data(year, month):
     df_data['Min Temp'] = temp_min
     df_data['Temp at 7:00'] = temp_dawn
     df_data['Rain'] = rain_value
+    df_data['Time Period of observation'] = date_period
 
     return df_data
 
